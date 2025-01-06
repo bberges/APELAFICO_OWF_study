a7feab717abf11fb7fd966d2731e90dbf8e78e36
Benoit Berges <benoit.berges@wur.nl>
2025-01-06 12:05:59 +0100

    update 2024 data

--- 1_read_data.R
+++ 1_read_data.R
@@ -60,6 +60,27 @@ if(processWBAT){
     # dir.phase <- dir.string[2]
     # dir.station <- dir.string[3]
     # dir.frequency   <- dir.string[4]
+    # options(warn=1)
+    # for(file in fileList){
+    #   print(file)
+    #   temp.list   <- strsplit(strsplit(file,'.txt')[[1]],'_')[[1]]
+    #   if(length(temp.list) == 6){
+    #     dataSet <- temp.list[1]
+    #     phase   <- 'P1'
+    #     station <- temp.list[2]
+    #     treshold    <- as.numeric(strsplit(temp.list[4],'=')[[1]][2])
+    #     frequency   <- as.numeric(strsplit(temp.list[3],'khz')[[1]][1])
+    #     chunk       <- as.numeric(strsplit(temp.list[5],'=')[[1]][2]) 
+    #   }else if(length(temp.list) == 7){
+    #     dataSet <- temp.list[1]
+    #     phase   <- temp.list[2]
+    #     station <- temp.list[3]
+    #     treshold    <- as.numeric(strsplit(temp.list[5],'=')[[1]][2])
+    #     frequency   <- as.numeric(strsplit(temp.list[4],'khz')[[1]][1])
+    #     chunk       <- as.numeric(strsplit(temp.list[6],'=')[[1]][2]) 
+    #   }
+    #   WBAT.current              <- fread(file.path(dataPath,'WBAT',myDir,file),skip=12,fill=TRUE)
+    # }
     
     for(file in fileList){
       print(file)
@@ -70,39 +91,39 @@ if(processWBAT){
         station <- temp.list[2]
         treshold    <- as.numeric(strsplit(temp.list[4],'=')[[1]][2])
         frequency   <- as.numeric(strsplit(temp.list[3],'khz')[[1]][1])
-        chunk       <- as.numeric(strsplit(temp.list[5],'=')[[1]][2]) 
+        chunk       <- as.numeric(strsplit(temp.list[5],'=')[[1]][2])
       }else if(length(temp.list) == 7){
         dataSet <- temp.list[1]
         phase   <- temp.list[2]
         station <- temp.list[3]
         treshold    <- as.numeric(strsplit(temp.list[5],'=')[[1]][2])
         frequency   <- as.numeric(strsplit(temp.list[4],'khz')[[1]][1])
-        chunk       <- as.numeric(strsplit(temp.list[6],'=')[[1]][2]) 
+        chunk       <- as.numeric(strsplit(temp.list[6],'=')[[1]][2])
       }
-      WBAT.current              <- fread(file.path(dataPath,'WBAT',myDir,file),skip="DATE")
-      
-      # fix of time strings  
+      WBAT.current              <- fread(file.path(dataPath,'WBAT',myDir,file),skip=12,fill=TRUE)
+
+      # fix of time strings
       n.char <- apply(WBAT.current[,'TIME'],1,nchar)
       WBAT.current$TIME[n.char == 7] <- paste0('0',WBAT.current$TIME[n.char == 7])
       WBAT.current$TIME[n.char == 5] <- paste0('000',WBAT.current$TIME[n.char == 5])
       WBAT.current$TIME[n.char == 4] <- paste0('0000',WBAT.current$TIME[n.char == 4])
-      
+
       WBAT.current$datetime <- as.POSIXct(paste0(WBAT.current$DATE,'T',WBAT.current$TIME),format="%Y%m%dT%H%M%S",tz="UTC")
-      
+
       # break into intervals
       thresh <- 60*5
-      WBAT.current <- WBAT.current %>% 
+      WBAT.current <- WBAT.current %>%
         arrange(datetime) %>%
         mutate(timediff = difftime(datetime, lag(datetime), units = 'secs'))
-      
+
       WBAT.current <- WBAT.current %>%
         mutate(timediff = ifelse(is.na(timediff), 1, timediff)) %>%
         mutate(thresh_pass = timediff > thresh,
                thresh_pass = as.numeric(thresh_pass)) %>%  # true = 1
         mutate(thresh_pass = ifelse(timediff==1, 1, thresh_pass))
-      
+
       WBAT.current$IDinter <- c(cumsum(WBAT.current$thresh_pass))
-      
+
       # tidy up data frame
       WBAT.current <- WBAT.current[WBAT.current$NoPelagic > 0,]
       WBAT.current <- WBAT.current %>% dplyr::select(-thresh_pass,-timediff,-DATE,-TIME,-Latitude,-Longitude,-START_LOG,-Species,-Frequency,-MinDepth,-MaxDepth,-Transcei.,-Threshold,-BottomUpper,-B_CH0)
@@ -119,7 +140,7 @@ if(processWBAT){
       WBAT.current$frequency  <- frequency
       WBAT.current$treshold   <- treshold
       WBAT.current$chunk      <- chunk
-      
+
       if(flagFirst){
         WBAT.all <- WBAT.current
         flagFirst <- F
@@ -127,15 +148,7 @@ if(processWBAT){
         WBAT.all <- rbind(WBAT.all,WBAT.current)
       }
     }
-    
-    # temp <- subset(WBAT.all,treshold == -50)
-    # temp <- subset(temp,depth == 0)
-    # 
-    # max(temp$datetime,na.rm=T)
-    # 
-    # ggplot(temp,aes(x=datetime,y=log10(SA)))+
-    #   geom_line()
-    
+
     save('WBAT.all',
          file = file.path(dataPath,paste0('WBAT_',myDir,'.RData')))
   } 
